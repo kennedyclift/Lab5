6703e9b63104bc79dfcc00b47c07d2a92db8c331
Kennedy Clift <u1286164@utah.edu>
2024-02-14 12:45:21 -0700

    Feb 14

--- lab5/views.py
+++ lab5/views.py
@@ -21,8 +21,9 @@ class HomePageView(View):
 
         if form.is_valid():
             my_message = form.cleaned_data['my_message']
+            service_selected = 'srvc' in form.cleaned_data['reviews_areas']
 
-        return render(request, self.template_name,{'form':form,'my_message':my_message})
+        return render(request, self.template_name,{'form':form,'my_message':my_message,'service_selected':service_selected})
         
 
 
